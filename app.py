a5f7d6fb4b46295fe9882f1d9303b7991cba888e
Jonah <61380701+jonahgluck@users.noreply.github.com>
2024-11-05 20:29:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import LinearRegression
 import scipy.stats as stats
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key_here"  # Replace with your own secret key for session management
+app.secret_key = "some_secret"
 
 def generate_data(N, mu, beta0, beta1, sigma2, S):
     # Generate a random dataset X of size N with values between 0 and 1
